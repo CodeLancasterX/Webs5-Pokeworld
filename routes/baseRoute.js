c5b55a3ecae69f457533291385cab102072d68d1
CodeLancasterX <Lancaster.mmxv@gmail.com>
2022-03-08 18:33:07 +0100

    heroku test

--- routes/baseRoute.js
+++ routes/baseRoute.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     const pokeWorldRoutes = {
-        routes: [{userRoutes: ['get_all_users: / ', 'get_user_by_id: /:id', 'get_encounter_by_userId: /:userId/encounters' ]}]
+        routes: [{userRoutes: ['get_all_users: / ', 'get_user_by_id: /:id', 'get_encounter_by_userId: /:userId/encounters' ]}],
+        message: "this is working."
     }
 
     res.status(200).json({
